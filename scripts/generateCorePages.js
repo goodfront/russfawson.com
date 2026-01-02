cf82b2e2da878572d7b93bc3ca4587e32ae795eb
Abe Fawson <46330941+afawson@users.noreply.github.com>
2026-01-02 13:20:52 -0700

    fix: make redirect pages html pages so we don't get weird downloaded php files.

--- scripts/generateCorePages.js
+++ scripts/generateCorePages.js
@@ -35,17 +35,17 @@ async function generateRedirects(distDir) {
   fs.mkdirSync(saddlesDir, { recursive: true });
 
   console.log('   Creating redirect: /saddles/photos.php -> /saddles/');
-  const saddlesPhotosPath = path.join(saddlesDir, 'photos.php');
+  const saddlesPhotosPath = path.join(saddlesDir, 'photos.php.html');
   fs.writeFileSync(saddlesPhotosPath, generateRedirect('/saddles/', 'Redirecting to saddle gallery...'), 'utf-8');
   results.push(saddlesPhotosPath);
 
   console.log('   Creating redirect: /saddles/options.php -> /saddles/options.html');
-  const saddlesOptionsPath = path.join(saddlesDir, 'options.php');
+  const saddlesOptionsPath = path.join(saddlesDir, 'options.php.html');
   fs.writeFileSync(saddlesOptionsPath, generateRedirect('/saddles/options.html', 'Redirecting to saddle options...'), 'utf-8');
   results.push(saddlesOptionsPath);
 
   console.log('   Creating redirect: /saddles/prices.php -> /saddles/prices.html');
-  const saddlesPricesPath = path.join(saddlesDir, 'prices.php');
+  const saddlesPricesPath = path.join(saddlesDir, 'prices.php.html');
   fs.writeFileSync(saddlesPricesPath, generateRedirect('/saddles/prices.html', 'Redirecting to saddle prices...'), 'utf-8');
   results.push(saddlesPricesPath);
 
@@ -54,17 +54,17 @@ async function generateRedirects(distDir) {
   fs.mkdirSync(accessoriesDir, { recursive: true });
 
   console.log('   Creating redirect: /accessories/index.php -> /accessories/photos.html');
-  const accessoriesIndexPath = path.join(accessoriesDir, 'index.php');
+  const accessoriesIndexPath = path.join(accessoriesDir, 'index.php.html');
   fs.writeFileSync(accessoriesIndexPath, generateRedirect('/accessories/photos.html', 'Redirecting to accessories photos...'), 'utf-8');
   results.push(accessoriesIndexPath);
 
   console.log('   Creating redirect: /accessories/photos.php -> /accessories/photos.html');
-  const accessoriesPhotosPath = path.join(accessoriesDir, 'photos.php');
+  const accessoriesPhotosPath = path.join(accessoriesDir, 'photos.php.html');
   fs.writeFileSync(accessoriesPhotosPath, generateRedirect('/accessories/photos.html', 'Redirecting to accessories photos...'), 'utf-8');
   results.push(accessoriesPhotosPath);
 
   console.log('   Creating redirect: /accessories/prices.php -> /accessories/prices.html');
-  const accessoriesPricesPath = path.join(accessoriesDir, 'prices.php');
+  const accessoriesPricesPath = path.join(accessoriesDir, 'prices.php.html');
   fs.writeFileSync(accessoriesPricesPath, generateRedirect('/accessories/prices.html', 'Redirecting to accessories prices...'), 'utf-8');
   results.push(accessoriesPricesPath);
 
@@ -73,17 +73,17 @@ async function generateRedirects(distDir) {
   fs.mkdirSync(holstersDir, { recursive: true });
 
   console.log('   Creating redirect: /holsters/index.php -> /holsters/photos.html');
-  const holstersIndexPath = path.join(holstersDir, 'index.php');
+  const holstersIndexPath = path.join(holstersDir, 'index.php.html');
   fs.writeFileSync(holstersIndexPath, generateRedirect('/holsters/photos.html', 'Redirecting to holsters photos...'), 'utf-8');
   results.push(holstersIndexPath);
 
   console.log('   Creating redirect: /holsters/photos.php -> /holsters/photos.html');
-  const holstersPhotosPath = path.join(holstersDir, 'photos.php');
+  const holstersPhotosPath = path.join(holstersDir, 'photos.php.html');
   fs.writeFileSync(holstersPhotosPath, generateRedirect('/holsters/photos.html', 'Redirecting to holsters photos...'), 'utf-8');
   results.push(holstersPhotosPath);
 
   console.log('   Creating redirect: /holsters/prices.php -> /holsters/prices.html');
-  const holstersPricesPath = path.join(holstersDir, 'prices.php');
+  const holstersPricesPath = path.join(holstersDir, 'prices.php.html');
   fs.writeFileSync(holstersPricesPath, generateRedirect('/holsters/prices.html', 'Redirecting to holsters prices...'), 'utf-8');
   results.push(holstersPricesPath);
 
